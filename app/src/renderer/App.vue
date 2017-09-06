01d744aad5e8ae84cf9f64e983ef56ce06fdf50c
Chenggang Wang <chenggang.wang@designlogic.com.au>
2017-09-06 17:01:15 +1000

    back button in navigation

--- app/src/renderer/App.vue
+++ app/src/renderer/App.vue
@@ -1,35 +1,34 @@
 <template>
-  <div id="app">
-    <section class="hero is-primary">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">Vue Player</h1>
-        </div>
-      </div>
-    </section>
-    <nav class="navbar">
-      <div class="navbar-brand">
-        <router-link to="playlist" class="navbar-item">Playlist</router-link>
-        <router-link to="nowplaying" class="navbar-item">Now Playing</router-link>
-      </div>
-      <div class="navbar-menu">
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <p class="control has-icons-right">
-              <input class="input" type="text" placeholder="Search" v-model="query" v-on:keydown.enter.prevent='search'>
-              <span class="icon is-small is-right" @click="search">
+    <div id="app">
+        <section class="hero is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">Vue Player</h1>
+                </div>
+            </div>
+        </section>
+        <nav class="navbar">
+            <div class="navbar-brand">
+                <a class="navbar-item" @click="back"><i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+                <router-link to="playlist" class="navbar-item">Playlist</router-link>
+                <router-link to="nowplaying" class="navbar-item">Now Playing</router-link>
+                <div class="navbar-end">
+                    <div class="navbar-item">
+                        <p class="control has-icons-right">
+                            <input class="input" type="text" placeholder="Search" v-model="query" v-on:keydown.enter.prevent='search'>
+                            <span class="icon is-small is-right" @click="search">
                 <i class="fa fa-search"></i>
               </span>
-            </p>
-          </div>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <router-view></router-view>
         </div>
-      </div>
-    </nav>
-    <div class="container">
-      <router-view></router-view>
+        <player></player>
     </div>
-    <player></player>
-  </div>
 </template>
 <script>
 import Player from 'renderer/components/Player';
@@ -45,6 +44,9 @@ export default {
   methods: {
     search() {
       this.$router.push({ path: '/search', query: { q: this.query } });
+    },
+    back() {
+      this.$router.go(-1);
     },
   },
 };
