769f95a1b67a1c768647bd026e1b248ab14f4b4c
Chenggang Wang <chenggang.wang@designlogic.com.au>
2017-09-12 15:07:40 +1000

    update detail view

--- app/src/renderer/components/Detail.vue
+++ app/src/renderer/components/Detail.vue
@@ -1,13 +1,24 @@
 <template>
-  <section class="section">
-    <h3 class="title" v-text="track.title"></h3>
-    <figure class="image is-128x128">
-        <img  :src="track.artwork_url">
-    </figure>
-  </section>
+  <div>
+    <h3 class="title has-text-centered" v-text="track.title"></h3>
+    <img class="image" :src="track.artwork_url | large">
+  </div>
 </template>
 <script>
 export default {
   props: ['track'],
+  filters: {
+    large(value) {
+      if (value) return value.replace('-large', '-t300x300');
+      return 'http://via.placeholder.com/500?text=No+Image';
+    },
+  },
 };
 </script>
+<style>
+  .image {
+    max-height: 300px;
+    max-width: 300px;
+    margin: 0 auto;
+  }
+</style>
