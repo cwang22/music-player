c42aecdb43915c9699e9934138f10d39557ddf19
Chenggang Wang <chenggang.wang@designlogic.com.au>
2017-09-22 16:07:15 +1000

    add eslint vue plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,26 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
   globals: {
     __static: true
   },
   plugins: [
-    'html'
+    'vue', 'html'
   ],
   'rules': {
+    // override/add rules' settings here
+    'vue/valid-v-if': 'error',
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
